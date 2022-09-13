a2636efc6451111aab08071565852c33cd55deca
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-13 12:32:19 +0900

    ✨ :: 프로젝트 생성

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/TilUltimateMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.tilultimatemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilUltimateMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilUltimateMainApplication.class, args);
+	}
+
+}
