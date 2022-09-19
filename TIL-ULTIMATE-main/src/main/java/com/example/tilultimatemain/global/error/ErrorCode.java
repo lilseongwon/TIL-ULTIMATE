b1ac26e452da5e4e54488f52257f5c8186af8b66
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-19 14:56:11 +0900

    📝 :: ErrorCode

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/global/error/ErrorCode.java
@@ -0,0 +1,31 @@
+package com.example.tilultimatemain.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    PASSWORD_MISMATCH(401-1, "Password Mismatch"),
+
+    EXPIRED_JWT(401-2, "Expired Jwt"),
+    SIGNATURE_JWT(401-3, "Signature Jwt"),
+    INVALID_JWT(401-4, "Invalid Jwt"),
+
+    INVALID_REFRESH_TOKEN(401-5, "Invalid Refresh Token"),
+
+    AUTH_NOT_FOUND(404-1, "Auth Not Found"),
+
+    POST_NOT_FOUND(404-2, "Post Not Found"),
+
+    REFRESH_TOKEN_NOT_FOUND(404-3, "RefreshToken Not Found"),
+
+    EMAIL_EXIST(409, "Email Exist"),
+
+    ALREADY_BOOK_EXIST(409, "Already Post Exist"), //아직 사용 안할 것
+
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int status;
+    private final String message;
+}
