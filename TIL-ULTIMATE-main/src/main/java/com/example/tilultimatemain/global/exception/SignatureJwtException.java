bcec72e574df458339db2692cf1415c82386e8c5
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-26 09:27:44 +0900

    📝 :: SignatureJwtException
    서명처 다른 예외

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/global/exception/SignatureJwtException.java
@@ -0,0 +1,14 @@
+package com.example.tilultimatemain.global.exception;
+
+import com.example.tilultimatemain.global.error.CustomException;
+import com.example.tilultimatemain.global.error.ErrorCode;
+
+public class SignatureJwtException extends CustomException {
+
+    public static final SignatureJwtException EXCEPTION =
+            new SignatureJwtException();
+
+    private SignatureJwtException() {
+        super(ErrorCode.SIGNATURE_JWT);
+    }
+}
