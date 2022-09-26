1d189cb2a01e376fb07fb351b7ac013cb2cdefe7
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-26 09:27:16 +0900

    📝 :: ExpiredJwtException
    만료된 토큰 예외

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/global/exception/ExpiredJwtException.java
@@ -0,0 +1,14 @@
+package com.example.tilultimatemain.global.exception;
+
+import com.example.tilultimatemain.global.error.CustomException;
+import com.example.tilultimatemain.global.error.ErrorCode;
+
+public class ExpiredJwtException extends CustomException {
+
+    public static final ExpiredJwtException EXCEPTION =
+            new ExpiredJwtException();
+
+    private ExpiredJwtException() {
+        super(ErrorCode.EXPIRED_JWT);
+    }
+}
