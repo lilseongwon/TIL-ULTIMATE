00bd659fca7ce174e356bf26cfcb35754c11e1ff
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-27 23:30:52 +0900

    📝 :: FilterConfig

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/global/security/jwt/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.example.tilultimatemain.global.security.jwt;
+
+import com.example.tilultimatemain.global.error.ExceptionFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void configure(HttpSecurity builder) {
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        ExceptionFilter exceptionFilter = new ExceptionFilter(objectMapper);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(exceptionFilter, JwtTokenFilter.class);
+    }
+}
