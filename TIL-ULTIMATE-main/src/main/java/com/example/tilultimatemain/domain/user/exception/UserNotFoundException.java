79712976d4baefb502a9d6c3d8bd96e359c82a02
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-28 11:21:57 +0900

    ♻️ :: CustomException으로 변경

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/exception/UserNotFoundException.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/exception/UserNotFoundException.java
@@ -4,7 +4,7 @@ import com.example.tilultimatemain.global.error.CustomException;
 import com.example.tilultimatemain.global.error.ErrorCode;
 
 public class UserNotFoundException extends CustomException {
-    public static final UserNotFoundException EXCEPTION =
+    public static final CustomException EXCEPTION =
             new UserNotFoundException();
 
     private UserNotFoundException() {
