d82cf0f2b77d3cddce88e41120d79cbb9d9486bc
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-28 14:01:22 +0900

    ✨ :: 패스워드 인코더, 트랜잭션 추가

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/service/UserSignupService.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/service/UserSignupService.java
@@ -5,21 +5,25 @@ import com.example.tilultimatemain.domain.user.domain.repository.UserRepository;
 import com.example.tilultimatemain.domain.user.facade.UserFacade;
 import com.example.tilultimatemain.domain.user.presentation.request.UserSignupRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserSignupService {
     private final UserRepository userRepository;
     private final UserFacade userFacade;
+    private final PasswordEncoder encoder;
 
+    @Transactional
     public void execute(UserSignupRequest request) {
         userFacade.checkUserExist(request.getAccountId());
 
         userRepository.save(
                 User.builder()
                         .accountId(request.getAccountId())
-                        .password(request.getPassword())
+                        .password(encoder.encode(request.getPassword()))
                         .sex(request.getSex())
                         .build());
     }
