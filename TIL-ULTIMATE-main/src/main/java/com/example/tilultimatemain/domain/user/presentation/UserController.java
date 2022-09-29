ff075cae132931031c4518952215cf99da6d9933
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-29 09:59:23 +0900

    ✨ :: 로그인 로직 구현

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/UserController.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/UserController.java
@@ -1,6 +1,9 @@
 package com.example.tilultimatemain.domain.user.presentation;
 
+import com.example.tilultimatemain.domain.auth.presentation.response.TokenResponse;
+import com.example.tilultimatemain.domain.user.presentation.request.UserSigninRequest;
 import com.example.tilultimatemain.domain.user.presentation.request.UserSignupRequest;
+import com.example.tilultimatemain.domain.user.service.UserSigninService;
 import com.example.tilultimatemain.domain.user.service.UserSignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
     private final UserSignupService userSignupService;
+    private final UserSigninService userSigninService;
 
     @PostMapping("/signup")
     public void signUp(@RequestBody UserSignupRequest request) {
         userSignupService.execute(request);
     }
 
+    @PostMapping("/login")
+    public TokenResponse signIn(@RequestBody UserSigninRequest request) {
+        return userSigninService.execute(request);
+    }
 
 }
