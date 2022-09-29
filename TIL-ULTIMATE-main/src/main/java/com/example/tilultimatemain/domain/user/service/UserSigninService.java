90c2e922206508316ddf0296c4fcc0b9fbc26ed8
kim seongwon <seongwon1290@dsm.hs.kr>
2022-09-29 09:59:07 +0900

    📝 :: UserSigninService

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/service/UserSigninService.java
@@ -0,0 +1,38 @@
+package com.example.tilultimatemain.domain.user.service;
+
+import com.example.tilultimatemain.domain.auth.presentation.response.TokenResponse;
+import com.example.tilultimatemain.domain.user.domain.User;
+import com.example.tilultimatemain.domain.user.exception.PasswordMismatchException;
+import com.example.tilultimatemain.domain.user.facade.UserFacade;
+import com.example.tilultimatemain.domain.user.presentation.request.UserSigninRequest;
+import com.example.tilultimatemain.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserSigninService {
+    private final UserFacade userFacade;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public TokenResponse execute(UserSigninRequest request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        User user = userFacade.getUserByAccountId(email);
+
+        if(!passwordEncoder.matches(password, user.getPassword()))
+            throw PasswordMismatchException.EXCEPTION;
+
+        TokenResponse tokenResponse = jwtTokenProvider.generateTokens(email);
+
+        return TokenResponse.builder()
+                .accessToken(tokenResponse.getAccessToken())
+                .refreshToken(tokenResponse.getRefreshToken())
+                .build();
+    }
+}
