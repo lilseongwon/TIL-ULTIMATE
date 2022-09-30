5dd9e2cff8b53a80c4c011fe1f283afa80e3966a
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-01 01:54:14 +0900

    📝 :: OauthCotroller

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/auth/presentation/OauthController.java
@@ -0,0 +1,40 @@
+package com.example.tilultimatemain.domain.auth.presentation;
+
+import com.example.tilultimatemain.domain.auth.presentation.response.TokenResponse;
+import com.example.tilultimatemain.domain.auth.service.GoogleAuthService;
+import com.example.tilultimatemain.domain.auth.service.KakaoAuthService;
+import com.example.tilultimatemain.global.util.auth.QueryGoogleAuthLinkService;
+import com.example.tilultimatemain.global.util.auth.QueryKakaoAuthLinkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class OauthController {
+    private final GoogleAuthService googleAuthService;
+    private final QueryGoogleAuthLinkService queryGoogleAuthLinkService;
+    private final KakaoAuthService kakaoAuthService;
+    private final QueryKakaoAuthLinkService queryKakaoAuthLinkService;
+
+    @GetMapping("/google/auth")
+    public String queryGoogleAuthLink() {
+        return queryGoogleAuthLinkService.execute();
+    }
+
+    @GetMapping("/google/receiveCode")
+    public TokenResponse googleAuthLogin(@RequestParam("code") String code) {
+        return googleAuthService.execute(code);
+    }
+
+    @GetMapping("/kakao/auth")
+    public String queryKakaoAuthLink() {
+        return queryKakaoAuthLinkService.execute();
+    }
+
+    @GetMapping("/kakao/receiveCode")
+    public TokenResponse kakaoAuthLogin(@RequestParam("code") String code) {
+        return kakaoAuthService.execute(code);
+    }
+}
