a347542eae9786e93a9308cd62c0c25eea857500
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-01 01:53:36 +0900

    📝 :: GoogleAuth

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/infrastructure/feign/client/GoogleAuth.java
@@ -0,0 +1,14 @@
+package com.example.tilultimatemain.infrastructure.feign.client;
+
+import com.example.tilultimatemain.infrastructure.feign.dto.request.GoogleCodeRequest;
+import com.example.tilultimatemain.infrastructure.feign.dto.response.TokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "GoogleAuthClient", url = "https://oauth2.googleapis.com/token")
+public interface GoogleAuth {
+
+    @PostMapping
+    TokenResponse googleAuth(GoogleCodeRequest googleCodeRequest);
+
+}
