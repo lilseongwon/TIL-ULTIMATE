31f0892f7df6e3b4b0374cad44d61e974bbc57b7
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-01 01:53:43 +0900

    📝 :: KakaoProperties

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/global/security/KakaoProperties.java
@@ -0,0 +1,24 @@
+package com.example.tilultimatemain.global.security;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConstructorBinding
+@ConfigurationProperties("auth.naver")
+public class KakaoProperties {
+
+    private final String baseUrl;
+    private final String clientId;
+    private final String clientSecret;
+    private final String redirectUrl;
+
+
+    public KakaoProperties(String baseUrl, String clientId, String clientSecret, String redirectUrl) {
+        this.baseUrl = baseUrl;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUrl = redirectUrl;
+    }
+}
