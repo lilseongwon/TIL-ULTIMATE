8bd7263931da697c3766ff1400a2c027745cd3c8
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-01 01:55:04 +0900

    ⚡ :: 카카오, 구글도 프로퍼티 활성화

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/global/properties/EnableConfigurationPropertiesConfig.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/global/properties/EnableConfigurationPropertiesConfig.java
@@ -1,8 +1,10 @@
 package com.example.tilultimatemain.global.properties;
 
+import com.example.tilultimatemain.global.security.AuthProperties;
+import com.example.tilultimatemain.global.security.KakaoProperties;
 import com.example.tilultimatemain.global.security.jwt.JwtProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(value = {JwtProperties.class})
+@EnableConfigurationProperties(value = {JwtProperties.class, AuthProperties.class, KakaoProperties.class})
 public class EnableConfigurationPropertiesConfig {
 }
