ee623a905542ac06d838dec7ccd294fa40172796
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-01 01:44:18 +0900

    📝 :: TokenResponse(Feign)

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/infrastructure/feign/dto/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.example.tilultimatemain.infrastructure.feign.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+
+}
