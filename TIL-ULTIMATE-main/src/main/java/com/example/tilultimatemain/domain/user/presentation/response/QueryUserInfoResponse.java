1990145688b9c512b67097321d1fa88920c3af37
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 08:18:04 +0900

    📝 :: QueryUserInfoResponse

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/response/QueryUserInfoResponse.java
@@ -0,0 +1,13 @@
+package com.example.tilultimatemain.domain.user.presentation.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QueryUserInfoResponse {
+
+    private final String email;
+    private final String name;
+    private final Integer money;
+}
