12d680e81f3ff70117ecb3e5f2a825b46b51045a
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 08:02:17 +0900

    ✨ :: 비번변경 기능 구현

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/UserController.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/UserController.java
@@ -3,8 +3,10 @@ package com.example.tilultimatemain.domain.user.presentation;
 import com.example.tilultimatemain.domain.auth.presentation.response.TokenResponse;
 import com.example.tilultimatemain.domain.user.presentation.request.UserSigninRequest;
 import com.example.tilultimatemain.domain.user.presentation.request.UserSignupRequest;
+import com.example.tilultimatemain.domain.user.presentation.request.UserUpdatePwRequest;
 import com.example.tilultimatemain.domain.user.service.UserSigninService;
 import com.example.tilultimatemain.domain.user.service.UserSignupService;
+import com.example.tilultimatemain.domain.user.service.UserUpdatePwService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserSignupService userSignupService;
     private final UserSigninService userSigninService;
+    private final UserUpdatePwService userUpdatePwService;
 
     @PostMapping("/signup")
     public void signUp(@RequestBody UserSignupRequest request) {
@@ -25,4 +28,8 @@ public class UserController {
         return userSigninService.execute(request);
     }
 
+    @PatchMapping("/password")
+    public void updatePw(@RequestBody UserUpdatePwRequest request) {
+        userUpdatePwService.execute(request);
+    }
 }
