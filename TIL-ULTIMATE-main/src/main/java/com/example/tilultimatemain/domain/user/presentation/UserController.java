6d5d14df96eabc0e0bd17ca33b31bc7f6a8e7d9f
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 08:09:37 +0900

    ✨ :: 회원탈퇴 기능 구현

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/UserController.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/UserController.java
@@ -7,7 +7,9 @@ import com.example.tilultimatemain.domain.user.presentation.request.UserUpdatePw
 import com.example.tilultimatemain.domain.user.service.UserSigninService;
 import com.example.tilultimatemain.domain.user.service.UserSignupService;
 import com.example.tilultimatemain.domain.user.service.UserUpdatePwService;
+import com.example.tilultimatemain.domain.user.service.WithdrawlService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -17,19 +19,29 @@ public class UserController {
     private final UserSignupService userSignupService;
     private final UserSigninService userSigninService;
     private final UserUpdatePwService userUpdatePwService;
+    private final WithdrawlService withdrawlService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     public void signUp(@RequestBody UserSignupRequest request) {
         userSignupService.execute(request);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("/login")
     public TokenResponse signIn(@RequestBody UserSigninRequest request) {
         return userSigninService.execute(request);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/password")
     public void updatePw(@RequestBody UserUpdatePwRequest request) {
         userUpdatePwService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/")
+    public void withdrawl() {
+        withdrawlService.execute();
     }
 }
