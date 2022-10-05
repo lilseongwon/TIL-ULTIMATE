217d07e0e96c525f8683aa53dce8864227944765
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 15:37:54 +0900

    📝 :: UpdateFoodService

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/service/UpdateFoodService.java
@@ -0,0 +1,22 @@
+package com.example.tilultimatemain.domain.food.service;
+
+import com.example.tilultimatemain.domain.food.domain.Food;
+import com.example.tilultimatemain.domain.food.facade.FoodFacade;
+import com.example.tilultimatemain.domain.food.presentation.request.FoodRequest;
+import com.example.tilultimatemain.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateFoodService {
+    private final UserFacade userFacade;
+    private final FoodFacade foodFacade;
+
+    public void execute(Long id, FoodRequest request) {
+        userFacade.checkPermission();
+        Food food = foodFacade.getFoodById(id);
+        food.updateFood(request.getName(), request.getCategory(),
+                request.getStock(), request.getExplain(), request.getPicture());
+    }
+}
