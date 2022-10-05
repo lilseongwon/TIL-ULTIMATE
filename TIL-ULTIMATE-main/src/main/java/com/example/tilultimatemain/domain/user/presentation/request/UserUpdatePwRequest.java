7ac03564c47da9e599c6f787f666f46c1db8bbfd
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 09:04:32 +0900

    ⚡ :: 비밀번호 제약조건 추가

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/request/UserUpdatePwRequest.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/request/UserUpdatePwRequest.java
@@ -3,9 +3,18 @@ package com.example.tilultimatemain.domain.user.presentation.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @NoArgsConstructor
 public class UserUpdatePwRequest {
+
+    @NotBlank(message = "password는 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+-=?/])[a-zA-Z0-9~!@#$%^&*()_+-=?/]{8,30}$", message = "password는 8-30자여야합니다.")
     private String password;
+
+    @NotBlank(message = "password는 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+-=?/])[a-zA-Z0-9~!@#$%^&*()_+-=?/]{8,30}$", message = "password는 8-30자여야합니다.")
     private String newPassword;
 }
