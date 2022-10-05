f90fc33533d20daddc0833105c042658a834572a
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 11:58:35 +0900

    📝 :: AddFoodService

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/service/AddFoodService.java
@@ -0,0 +1,32 @@
+package com.example.tilultimatemain.domain.food.service;
+
+import com.example.tilultimatemain.domain.food.domain.Food;
+import com.example.tilultimatemain.domain.food.facade.FoodFacade;
+import com.example.tilultimatemain.domain.food.domain.repository.FoodRepository;
+import com.example.tilultimatemain.domain.food.presentation.request.AddFoodRequest;
+import com.example.tilultimatemain.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class AddFoodService {
+    private final FoodRepository foodRepository;
+    private final UserFacade userFacade;
+    private final FoodFacade foodFacade;
+
+    @Transactional
+    public void addFood(AddFoodRequest request) {
+        userFacade.checkPermission();
+        foodFacade.checkFoodExist(request.getName());
+
+        foodRepository.save(
+                Food.builder()
+                        .name(request.getName())
+                        .category(request.getCategory())
+                        .explain(request.getExplain())
+                        .picture(request.getPicture())
+                        .build());
+    }
+}
