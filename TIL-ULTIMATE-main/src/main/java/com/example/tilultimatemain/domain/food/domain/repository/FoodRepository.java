f3a8ec50eca517a553cf568a1e4160b455744ddf
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 15:35:57 +0900

    📝 :: FoodRepository

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/domain/repository/FoodRepository.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/domain/repository/FoodRepository.java
@@ -1,10 +1,17 @@
 package com.example.tilultimatemain.domain.food.domain.repository;
 
+import com.example.tilultimatemain.domain.food.domain.Category;
 import com.example.tilultimatemain.domain.food.domain.Food;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FoodRepository extends CrudRepository<Food, Long> {
     Optional<Food> findByName(String name);
+    List<Food> findAllByOrderByIdDesc();
+
+    List<Food> findAllByCategoryContains(Category category);
+
+    List<Food> findAllByNameContains(String keyword);
 }
