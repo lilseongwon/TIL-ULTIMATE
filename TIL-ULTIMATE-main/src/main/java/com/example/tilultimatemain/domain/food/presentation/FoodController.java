10636bc09a210998e7df8adc1f7dcf64ee5df585
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 15:40:40 +0900

    ✨ :: 음식정보 업데이트 기능 구현

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/presentation/FoodController.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/presentation/FoodController.java
@@ -2,21 +2,28 @@ package com.example.tilultimatemain.domain.food.presentation;
 
 import com.example.tilultimatemain.domain.food.presentation.request.FoodRequest;
 import com.example.tilultimatemain.domain.food.service.AddFoodService;
+import com.example.tilultimatemain.domain.food.service.UpdateFoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
 public class FoodController {
     private final AddFoodService addFoodService;
+    private final UpdateFoodService updateFoodService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/")
     public void addFood(@RequestBody FoodRequest request) {
         addFoodService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{food-id}")
+    public void execute(@PathVariable("food-id") Long id, @RequestBody @Valid FoodRequest foodRequest) {
+        updateFoodService.execute(id, foodRequest);
     }
 }
