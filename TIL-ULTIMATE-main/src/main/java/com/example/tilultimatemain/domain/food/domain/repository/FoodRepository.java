52903b6fe5a667e0f8565826e2454020f9e373ea
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-05 11:57:58 +0900

    ⚡ :: findByName 추가

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/domain/repository/FoodRepository.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/domain/repository/FoodRepository.java
@@ -3,6 +3,8 @@ package com.example.tilultimatemain.domain.food.domain.repository;
 import com.example.tilultimatemain.domain.food.domain.Food;
 import org.springframework.data.repository.CrudRepository;
 
-public interface FoodRepository extends CrudRepository<Food, Long> {
+import java.util.Optional;
 
+public interface FoodRepository extends CrudRepository<Food, Long> {
+    Optional<Food> findByName(String name);
 }
