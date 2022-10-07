2c39d12437e3543eed23b9141b20f97ecec43b9b
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-07 15:54:13 +0900

    📝 :: AddOrderService

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/service/AddOrderService.java
@@ -0,0 +1,34 @@
+package com.example.tilultimatemain.domain.order.service;
+
+import com.example.tilultimatemain.domain.food.domain.Food;
+import com.example.tilultimatemain.domain.food.facade.FoodFacade;
+import com.example.tilultimatemain.domain.order.domain.Order;
+import com.example.tilultimatemain.domain.order.domain.repository.OrderRepository;
+import com.example.tilultimatemain.domain.order.facade.OrderFacade;
+import com.example.tilultimatemain.domain.order.presentation.request.AddOrderRequest;
+import com.example.tilultimatemain.domain.user.domain.User;
+import com.example.tilultimatemain.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AddOrderService {
+    private final OrderRepository orderRepository;
+    private final UserFacade userFacade;
+    private final FoodFacade foodFacade;
+    private final OrderFacade orderFacade;
+
+    public void addOrder(Long id, AddOrderRequest request) {
+        User user = userFacade.getCurrentUser();
+        Food food = foodFacade.getFoodById(id);
+
+        orderRepository.save(Order
+                .builder()
+                .foodId(food)
+                .userId(user)
+                .sum(request.getSum())
+                .price(orderFacade.getPrice(id, request.getSum()))
+                .build());
+    }
+}
