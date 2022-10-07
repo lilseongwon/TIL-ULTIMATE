21889fdd7ee221cb542c6037ff5e7535cb659152
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-07 15:54:23 +0900

    📝 :: AddOrderRequest

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/presentation/request/AddOrderRequest.java
@@ -0,0 +1,10 @@
+package com.example.tilultimatemain.domain.order.presentation.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AddOrderRequest {
+    private Integer sum;
+}
