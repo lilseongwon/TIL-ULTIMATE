76148be0465c241e1e2dbfbd94ee189f947b1df9
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-07 12:50:37 +0900

    📝 :: QueryFoodResponse

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/presentation/response/QueryFoodResponse.java
@@ -0,0 +1,22 @@
+package com.example.tilultimatemain.domain.food.presentation.response;
+
+import com.example.tilultimatemain.domain.food.domain.Category;
+import com.example.tilultimatemain.domain.food.domain.Food;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QueryFoodResponse {
+    private final String name;
+    private final Category category;
+    private final String picture;
+
+    public static QueryFoodResponse of(Food food) {
+        return QueryFoodResponse.builder().
+                name(food.getName())
+                .category(food.getCategory())
+                .picture(food.getPicture())
+                .build();
+    }
+}
