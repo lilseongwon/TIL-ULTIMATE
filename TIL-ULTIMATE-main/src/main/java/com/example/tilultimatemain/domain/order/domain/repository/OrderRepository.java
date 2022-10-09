3631edcea4e7c79eb33f281d6bdd62950fadf108
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-09 17:52:58 +0900

    ⚡ :: 해당날짜 주문찾기 메소드

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/domain/repository/OrderRepository.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/domain/repository/OrderRepository.java
@@ -3,5 +3,10 @@ package com.example.tilultimatemain.domain.order.domain.repository;
 import com.example.tilultimatemain.domain.order.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findAllByCreatedAt(LocalDate createdAt);
 }
