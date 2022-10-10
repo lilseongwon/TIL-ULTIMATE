1865d76d34b2d679f1e4a77bf6de82f8b56ce071
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-11 08:33:25 +0900

    ♻️ :: 테이블 orders로 매핑
    sql 예약어라서 바꿔야했음

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/domain/Order.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/domain/Order.java
@@ -13,17 +13,18 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "orders")
 public class Order extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "food_id", nullable = false)
+    @JoinColumn(name = "food", nullable = false)
     private Food foodId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user", nullable = false)
     private User userId;
 
     @Column(nullable = false)
