afbe65da257ee3848c89f055bf77ada1c90e7a91
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-11 08:29:25 +0900

    ⚡ :: Order 영속성 전이 (Remove)

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/domain/Food.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/domain/Food.java
@@ -1,15 +1,18 @@
 package com.example.tilultimatemain.domain.food.domain;
 
+import com.example.tilultimatemain.domain.order.domain.Order;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "food")
 public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +28,9 @@ public class Food {
     private String picture;
 
     private Integer price;
+
+    @OneToMany(mappedBy = "food", cascade = CascadeType.REMOVE)
+    private List<Order> orderList;
 
     @Builder
     public Food(String name, Category category, String explain, String picture, Integer price) {
