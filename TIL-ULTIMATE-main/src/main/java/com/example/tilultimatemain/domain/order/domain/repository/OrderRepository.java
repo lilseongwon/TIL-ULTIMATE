5e9b5870db32b915ec8ce039749069632199c346
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-11 08:38:33 +0900

    ⚡ :: userid 메소드 추가

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/domain/repository/OrderRepository.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/domain/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.example.tilultimatemain.domain.order.domain.repository;
 
 import com.example.tilultimatemain.domain.order.domain.Order;
+import com.example.tilultimatemain.domain.order.domain.Status;
+import com.example.tilultimatemain.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
@@ -8,5 +10,6 @@ import java.util.List;
 
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
-    List<Order> findAllByCreatedAt(LocalDate createdAt);
+    List<Order> findAllByCreatedAtAndStatus(LocalDate createdAt, Status status);
+    List<Order> findAllByUserId(User user);
 }
