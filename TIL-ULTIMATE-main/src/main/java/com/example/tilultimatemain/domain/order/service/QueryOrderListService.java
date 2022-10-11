3f6949dae8cb4ddb1604bd72acb92dbb33eca0ab
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-11 20:18:17 +0900

    📝 :: QueryOrderListService

---
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/service/QueryOrderListService.java
@@ -0,0 +1,29 @@
+package com.example.tilultimatemain.domain.order.service;
+
+import com.example.tilultimatemain.domain.order.domain.Status;
+import com.example.tilultimatemain.domain.order.domain.repository.OrderRepository;
+import com.example.tilultimatemain.domain.order.presentation.response.QueryOrderListResponse;
+import com.example.tilultimatemain.domain.order.presentation.response.QueryOrderResponse;
+import com.example.tilultimatemain.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QueryOrderListService {
+    private final UserFacade userFacade;
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public QueryOrderListResponse execute() {
+        userFacade.checkPermission();
+        List<QueryOrderResponse> orderResponseList = orderRepository.findAllByCreatedAtAndStatus(LocalDate.now(), Status.COOK)
+                .stream()
+                .map(QueryOrderResponse::of).toList();
+        return new QueryOrderListResponse(orderResponseList);
+    }
+}
