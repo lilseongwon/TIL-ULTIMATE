e4f51f54618ed5b29a435783024e20d371e6f060
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-12 08:25:50 +0900

    ⚡ :: 수량 필드에 제약조건 추가

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/presentation/request/AddOrderRequest.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/presentation/request/AddOrderRequest.java
@@ -3,8 +3,12 @@ package com.example.tilultimatemain.domain.order.presentation.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class AddOrderRequest {
+
+    @NotNull(message = "sum은 Null을 허용하지 않습니다")
     private Integer sum;
 }
