a6cda7c119eb5edd3eaf3d09cd5724a8740d5e37
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-12 13:42:37 +0900

    ⚡ :: getOrderById 메소드 추가

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/facade/OrderFacade.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/facade/OrderFacade.java
@@ -2,6 +2,9 @@ package com.example.tilultimatemain.domain.order.facade;
 
 import com.example.tilultimatemain.domain.food.domain.Food;
 import com.example.tilultimatemain.domain.food.facade.FoodFacade;
+import com.example.tilultimatemain.domain.order.domain.Order;
+import com.example.tilultimatemain.domain.order.domain.repository.OrderRepository;
+import com.example.tilultimatemain.domain.order.exception.OrderNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderFacade {
     private final FoodFacade foodFacade;
+    private final OrderRepository orderRepository;
 
     public Integer getPrice(Long id, Integer sum) {
         Food food = foodFacade.getFoodById(id);
         return food.getPrice() * sum;
+    }
+
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> OrderNotFoundException.EXCEPTION);
     }
 }
