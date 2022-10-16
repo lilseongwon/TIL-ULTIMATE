4b3dbd10e9a83a46cfe052165b2415cb68000863
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-16 23:33:19 +0900

    ⚡ :: of 함수 추가, dto로 use조회

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/presentation/response/QueryOrderResponse.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/presentation/response/QueryOrderResponse.java
@@ -1,9 +1,11 @@
 package com.example.tilultimatemain.domain.order.presentation.response;
 
 import com.example.tilultimatemain.domain.food.domain.Food;
+import com.example.tilultimatemain.domain.food.presentation.response.QueryFoodResponse;
 import com.example.tilultimatemain.domain.order.domain.Order;
 import com.example.tilultimatemain.domain.order.domain.Status;
 import com.example.tilultimatemain.domain.user.domain.User;
+import com.example.tilultimatemain.domain.user.presentation.response.QueryUserInfoResponse;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,16 +14,16 @@ import java.time.LocalDate;
 @Getter
 @Builder
 public class QueryOrderResponse {
-    private final Food food;
-    private final User user;
+    private final QueryFoodResponse food;
+    private final QueryUserInfoResponse user;
     private final LocalDate createdAt;
     private final Status status;
 
     public static QueryOrderResponse of(Order order) {
         return QueryOrderResponse.builder()
                 .createdAt(order.getCreatedAt())
-                .food(order.getFood())
-                .user(order.getUser())
+                .food(QueryFoodResponse.of(order.getFood()))
+                .user(QueryUserInfoResponse.of(order.getUser()))
                 .status(order.getStatus()).build();
     }
 }
--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/response/QueryUserInfoResponse.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/user/presentation/response/QueryUserInfoResponse.java
@@ -1,5 +1,6 @@
 package com.example.tilultimatemain.domain.user.presentation.response;
 
+import com.example.tilultimatemain.domain.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,4 +10,11 @@ public class QueryUserInfoResponse {
 
     private final String email;
     private final String name;
+
+    public static QueryUserInfoResponse of(User user) {
+        return QueryUserInfoResponse.builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+    }
 }
