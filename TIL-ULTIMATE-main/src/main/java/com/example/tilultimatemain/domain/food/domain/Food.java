5595d2d95861129952c3ee4f657bf073618aa31b
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-16 18:48:53 +0900

    ⚡ :: Enumerate(String)

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/domain/Food.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/food/domain/Food.java
@@ -1,6 +1,7 @@
 package com.example.tilultimatemain.domain.food.domain;
 
 import com.example.tilultimatemain.domain.order.domain.Order;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "food")
+@Table(name = "foods")
 public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +22,7 @@ public class Food {
     @Column(length = 50, nullable = false)
     private String name;
 
+    @Enumerated(EnumType.STRING)
     private Category category;
 
     private String explain;
@@ -28,9 +30,6 @@ public class Food {
     private String picture;
 
     private Integer price;
-
-    @OneToMany(mappedBy = "food", cascade = CascadeType.REMOVE)
-    private List<Order> orderList;
 
     @Builder
     public Food(String name, Category category, String explain, String picture, Integer price) {
