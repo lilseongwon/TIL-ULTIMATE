9e0e9d4377d1d80fcc9cb7d91cdc9dae7b50f6bc
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-16 23:31:48 +0900

    ⚡ :: save 메소드 추가
    status 변경 안되는 문제 해결

--- TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/service/UpdateOrderStatusService.java
+++ TIL-ULTIMATE-main/src/main/java/com/example/tilultimatemain/domain/order/service/UpdateOrderStatusService.java
@@ -1,6 +1,7 @@
 package com.example.tilultimatemain.domain.order.service;
 
 import com.example.tilultimatemain.domain.order.domain.Order;
+import com.example.tilultimatemain.domain.order.domain.repository.OrderRepository;
 import com.example.tilultimatemain.domain.order.facade.OrderFacade;
 import com.example.tilultimatemain.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +12,12 @@ import org.springframework.stereotype.Service;
 public class UpdateOrderStatusService {
     private final UserFacade userFacade;
     private final OrderFacade orderFacade;
+    private final OrderRepository orderRepository;
 
     public void execute(Long id) {
         userFacade.checkPermission();
         Order order = orderFacade.getOrderById(id);
         order.completeCook();
+        orderRepository.save(order);
     }
 }
